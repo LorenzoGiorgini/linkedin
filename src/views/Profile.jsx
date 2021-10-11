7644ba40a0310e36df0f980cb4debe96b9161328
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-11 10:03:00 +0200

    layout
    create-react-app

---
+++ src/views/Profile.jsx
@@ -0,0 +1,20 @@
+import { Container , Row , Col } from "react-bootstrap"
+
+
+
+
+
+const Profile = () => (
+    <Container>
+        <Row>
+            <Col md={8} className="color">
+                <h1>dsdfdsfsf</h1>
+            </Col>
+            <Col md={4} className="color">
+                <h1>dsdfdsfsf</h1>
+            </Col>
+        </Row>
+    </Container>
+)
+
+export default Profile
