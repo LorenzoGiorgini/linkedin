19f928b784dacabd0893687b5d57dfd9fb667b91
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-11 16:50:54 +0200

    Update PopUp.jsx

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -3,7 +3,6 @@ import { useState , useEffect } from "react";
 
 const PopUp = () => {
 
-    const [ smShow, setSmShow ] = useState(false);
     const [ lgShow, setLgShow ] = useState(false);
     const [ updateUser , setUpdateUser ] = useState({})
 
@@ -16,19 +15,6 @@ const PopUp = () => {
             </svg>
         </div>
         <Modal
-            size="sm"
-            show={smShow}
-            onHide={() => setSmShow(false)}
-            aria-labelledby="example-modal-sizes-title-sm"
-        >
-            <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-sm">
-                Small Modal
-            </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>...</Modal.Body>
-        </Modal>
-        <Modal
             size="lg"
             show={lgShow}
             onHide={() => setLgShow(false)}
@@ -39,7 +25,11 @@ const PopUp = () => {
                 Large Modal
             </Modal.Title>
             </Modal.Header>
-            <Modal.Body>...</Modal.Body>
+            <Modal.Body>something</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => setLgShow(false)} >Close</Button>
+                <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
         </Modal>
         </>
     );
