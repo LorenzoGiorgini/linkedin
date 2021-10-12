0e602d3e9c0af7a48ac1ba4eb6feb8506d935a38
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-12 14:56:38 +0200

    changes in styles
    SingleExperience Component, ExperienceList component, changes oin styles, react bootstraps icons etc

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, Row, Col, Image } from "react-bootstrap";
+import { Plus } from "react-bootstrap-icons";
 import SingleExperience from "./SingleExperience";
 
 class ExperienceList extends Component {
@@ -10,14 +11,13 @@ class ExperienceList extends Component {
   componentDidMount = async () => {
     let userId = "616434dda890cc0015cf07f0";
     const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU";
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU";
     const endpointMyProfile = `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`;
 
     try {
       let fetchData = await fetch(endpointMyProfile, {
         headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU"
+          Authorization: token,
         },
       });
       let myExperienceData = await fetchData.json();
@@ -33,14 +33,21 @@ class ExperienceList extends Component {
   render() {
     const { experience } = this.state;
     return (
-      <Card>
+      <Card >
         <Card.Title>
           <span></span>
+          <Plus/>
         </Card.Title>
-        <Card.Body>
+        <Card.Body >
           {experience.map((element) => (
             <Col>
-              <SingleExperience />
+              <SingleExperience
+                role={element.role}
+                location={element.area}
+                date={element.startDate}
+                company={element.company}
+                desc={element.description}
+              />
             </Col>
           ))}
         </Card.Body>
--- src/components/SingleExperience.jsx
+++ src/components/SingleExperience.jsx
@@ -1,8 +1,28 @@
 import { Component } from "react";
+import { Card, Col, Row, Image } from "react-bootstrap";
+import { Pencil } from "react-bootstrap-icons";
+import "../CssStyles/Experience.css"
 
 class SingleExperience extends Component {
   render() {
-    return <h1>lorem ipsum lorem ipsum</h1>;
+    return (
+      <>
+        <hr className="mt-1 mb-2" />
+        <Pencil />
+        <Row id="experience-single-row">
+          <Col xs={2}>
+            <Image src="https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/1045/original/Strive_-_logosquareblack.png" />
+          </Col>
+          <Col xs={10}>
+            <p>{this.props.role}</p>
+            <p>{this.props.company}</p>
+            <p>{this.props.date}</p>
+            <p>{this.props.area}</p>
+            <p>{this.props.desc}</p>
+          </Col>
+        </Row>
+      </>
+    );
   }
 }
 
