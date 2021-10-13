834ef2eb7bfdf87f7bba4e05c716c46c043c81d2
adam-belahouane <adam_belahouane@outlook.com>
2021-10-13 11:32:55 +0100

    NewPost is working

--- src/components/NewPostModal.jsx
+++ src/components/NewPostModal.jsx
@@ -1,11 +1,40 @@
 import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Alert, Button, Col, Form, Modal, Row } from "react-bootstrap";
 
 const NewPostModal = () => {
   const [show, setShow] = useState(false);
+  const [newpost, setNewPost] = useState(null);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleInput = (propertyName, value) => {
+    setNewPost({
+      [propertyName]: value,
+    });
+  };
+  const makeNewPost = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/",
+        {
+          method: "POST",
+          body: JSON.stringify(newpost),
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU",
+            "Content-type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        <Alert>post successfull</Alert>;
+        console.log(newpost)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -20,14 +49,41 @@ const NewPostModal = () => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Create a post</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>
+                <Row>
+                  <Col sm={2}>
+                    <img
+                      className="img-fluid"
+                      style={{ width: "50px" }}
+                      src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                      alt=""
+                    />
+                  </Col>
+                  <Col col={10}>
+                    <h3>Lorenzo Giorgini</h3>
+                  </Col>
+                </Row>
+              </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={5}
+                onChange={(e) => handleInput("text", e.target.value)}
+                type="text"
+                placeholder="What do you want to talk about?"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={makeNewPost}>
             Save Changes
           </Button>
         </Modal.Footer>
