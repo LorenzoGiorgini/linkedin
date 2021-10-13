e1481e5a542b011818a9a351cf021e39dc11f207
adam-belahouane <adam_belahouane@outlook.com>
2021-10-13 10:38:22 +0100

    New Post

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,32 @@
+import { Row, Col, Button} from "react-bootstrap"
+
+const NewPost = () => {
+   return (
+       <>
+    <Row>
+        <Col sm={2}>
+            <img className="img-fluid" src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="" />
+        </Col>
+        <Col sm={10}>
+            <Button style={{width : '100%'}}>
+                Start a post
+            </Button>
+        </Col>
+    </Row>
+
+    <Row>
+        <Col sm={3}></Col>
+        <Col sm={3}></Col>
+        <Col sm={3}></Col>
+        <Col sm={3}></Col>
+
+    </Row>
+    
+    </>
+    
+    )
+
+
+}
+
+export default NewPost
