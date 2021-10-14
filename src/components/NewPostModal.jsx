f910cbc57de55bcce6f00517a99b906018e63480
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-14 15:26:41 +0200

    commits with Lorenzo

--- src/components/NewPostModal.jsx
+++ src/components/NewPostModal.jsx
@@ -2,24 +2,18 @@ import { useState } from "react";
 import { Alert, Button, Col, Form, Modal, Row } from "react-bootstrap";
 import "../CssStyles/NewPost.css";
 
-const NewPostModal = ({ fetchPosts , posts }) => {
-
-
+const NewPostModal = ({ fetchPosts }) => {
   const [show, setShow] = useState(false);
   const [newpost, setNewPost] = useState(null);
-  const [ imagePost , setImagePost ] = useState(null)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleInput = (propertyName, value) => {
     setNewPost({
-      ...newpost,
       [propertyName]: value,
     });
   };
-
-
   const makeNewPost = async () => {
     try {
       let response = await fetch(
@@ -39,43 +33,7 @@ const NewPostModal = ({ fetchPosts , posts }) => {
         fetchPosts();
         setShow(false);
         console.log(newpost);
-        submitFile()
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-  const [image, setImage] = useState(null);
-
-  const TargetFile = (e) => {
-    console.log("Event", e.target.files[0]);
-    if (e.target && e.target.files[0]) {
-      setImage(e.target.files[0]);
-      setImagePost(e.target.files[0])
-    }
-  };
-
-  const submitFile = async () => {
-    try {
-
-      let formData = new FormData();
-
-      formData.append("post", image);
-
-      let response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/posts/`,
-        {
-          body: formData,
-          method: "POST",
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU",
-          },
-        }
-      );
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -116,7 +74,6 @@ const NewPostModal = ({ fetchPosts , posts }) => {
                 placeholder="What do you want to talk about?"
               />
             </Form.Group>
-            <input type="file" onChange={TargetFile}/>
           </Form>
         </Modal.Body>
         <Modal.Footer>
