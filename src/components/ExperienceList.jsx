3e3cfb94fc0aa1728ed3ba8f17fbe92124a2aab8
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-16 13:30:21 +0000

    Removed headers

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -13,15 +13,7 @@ const ExperienceList = (props) => {
   
   const fetchUserExp = async () => {
     try {
-      let response = await fetch(
-        `https://striveschool-api.herokuapp.com/api/profile/${props.user._id}/experiences`,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTY0MzRkZGE4OTBjYzAwMTVjZjA3ZjAiLCJpYXQiOjE2MzM5NTcwODUsImV4cCI6MTYzNTE2NjY4NX0.0KiKm3Nj5tYFKqs2AZK3KMWJf7ldhr1wmccH_VdoyjU",
-          },
-        }
-      );
+      let response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${props.user._id}/experiences`,);
       if (response.ok) {
         let data = await response.json();
         props.setGetExperience(data);
