05ba8ac9056616b196652adc39a0bc98770f6c14
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-11-18 10:06:55 +0000

    comments

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,91 +1,178 @@
-import  Dropdown from "react-bootstrap/Dropdown";
+import Dropdown from "react-bootstrap/Dropdown";
 import "../CssStyles/Posts.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EditPostModule from "./EditPostModule";
+import { Link } from "react-router-dom";
+import AddComment from "./AddComment";
+import { Row, Col } from "react-bootstrap";
+
 
 const SinglePost = (props) => {
+  // console.log("This is element", props.element);
+
+  const [comments, setComments] = useState([]);
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const fetchComments = async () => {
+    try {
+      const response = await fetch(
+        `https://strive-linkedin.herokuapp.com/posts/${props.element._id}/comment`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        let array = await response.json();
+        setComments(array.data);
+        console.log("This array of comments", array.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
   const deleteSelectedPost = async () => {
     try {
-        let response = await fetch(`https://strive-linkedin.herokuapp.com/posts/${props.element._id}`,{method: "DELETE"})
-        props.fetchPosts()
+       let response = await fetch(
+        `https://strive-linkedin.herokuapp.com/posts/${props.element._id}`,
+        { method: "DELETE" }
+      );
+      props.fetchPosts();
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className="container-post">
-      <div className="padding-jumbo">
-        <div className="row">
-          <div className="image-container col-2 mt-3">
-            <div className="img-profile-container">
-              <img className="profile-img" src={props.element.user.image} />
+    <>
+      <div className="container-post">
+        <div className="padding-jumbo">
+          <div className="row">
+            <div className="image-container col-2 mt-3">
+              <div className="img-profile-container">
+                <img className="profile-img" src={props.element.user.image} />
+              </div>
             </div>
-          </div>
-          <div className="d-flex flex-column  mb-2 col-8 mt-3">
-            <span className="span-big">
-              {props.element.user.name} {props.element.user.surname}
-            </span>
+            <div className="d-flex flex-column  mb-2 col-8 mt-3">
+              <span className="span-big">
+                {props.element.user.name} {props.element.user.surname}
+              </span>
+              <span className="span-small text-muted">
+                {props.element.user.title}
+              </span>
 
-            <span className="span-small text-muted">
-              {props.element.user.title}
-            </span>
-
-            <span className="span-small text-muted">
-              {props.element.user.createdAt}
-            </span>
+              <span className="span-small text-muted">
+                {props.element.user.createdAt}
+              </span>
             </div>
             <div className="col-2 mt-3">
-          <Dropdown>
-            <Dropdown.Toggle className="dropdown-styled">
-              <i class="bi bi-three-dots" style={{fontSize:"25px"}} ></i>
-            </Dropdown.Toggle>
-
-            {props.element.user._id === "619234e538541a787a13c554" ? (
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1" onClick={handleShow}>
-                  Modify
-                </Dropdown.Item>
-                <EditPostModule
-                  show={show}
-                  onHide={handleClose}
-                  id={props.element._id}
-                  fetchPosts={props.fetchPosts}
-                  profile={props.profile}
-                />
-                <Dropdown.Item href="#/action-2" onClick={deleteSelectedPost}>Delete Post</Dropdown.Item>
-                <Dropdown.Item href="#/action-1">Save</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Copy Link</Dropdown.Item>
-              </Dropdown.Menu>
-            ) : (
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Save</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Copy Link</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Report</Dropdown.Item>
-              </Dropdown.Menu>
-            )}
-          </Dropdown>
+              <Dropdown>
+                <Dropdown.Toggle className="dropdown-styled">
+                  <i class="bi bi-three-dots" style={{ fontSize: "25px" }}></i>
+                </Dropdown.Toggle>
+                {props.element.user._id === "619234e538541a787a13c554" ? (
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1" onClick={handleShow}>
+                      Modify
+                    </Dropdown.Item>
+                    <EditPostModule
+                      show={show}
+                      onHide={handleClose}
+                      id={props.element._id}
+                      fetchPosts={props.fetchPosts}
+                      profile={props.profile}
+                    />
+                    <Dropdown.Item
+                      href="#/action-2"
+                      onClick={deleteSelectedPost}
+                    >
+                      Delete Post
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-1">Save</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Copy Link</Dropdown.Item>
+                  </Dropdown.Menu>
+                ) : (
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Save</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Copy Link</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Report</Dropdown.Item>
+                  </Dropdown.Menu>
+                )}
+              </Dropdown>
+            </div>
           </div>
-        </div>
-        <div className="post-description">{props.element.text}</div>
-        {props.element.image && <div>
-          <img className="image-container" src={props.element.image} alt=" " />
-        </div>}
-        <div className="translation">See Translation</div>
-        <hr />
-        <div className="d-flex justify-content-around">
-          <span className="feed-buttons"><i class="bi bi-hand-thumbs-up mr-2"></i>Like</span>
-          <span className="feed-buttons"><i class="bi bi-chat-text mr-2"></i>Comment</span>
-          <span className="feed-buttons"><i class="bi bi-share-fill mr-2"></i>Share</span>
-          <span className="feed-buttons"><i class="bi bi-cursor-fill mr-2"></i>Send</span>
+          <div className="post-description">{props.element.text}</div>
+          {props.element.image && (
+            <div>
+              <img
+                className="image-container"
+                src={props.element.image}
+                alt=" "
+              />
+            </div>
+          )}
+          <div className="translation">See Translation</div>
+          <hr />
+          <div className="d-flex justify-content-around">
+            <span className="feed-buttons">
+              <i class="bi bi-hand-thumbs-up mr-2"></i>Like
+            </span>
+            <div className="feed-buttons" style={{ cursor: "pointer" }}>
+              <AddComment
+                id={props.element._id}
+                userId={props.element.user._id}
+                fetchComments={fetchComments}
+              />
+              <span> Comment</span>
+            </div>
+            <span className="feed-buttons">
+              <i class="bi bi-share-fill mr-2"></i>Share
+            </span>
+            <span className="feed-buttons">
+              <i class="bi bi-cursor-fill mr-2"></i>Send
+            </span>
+          </div>
+          {comments.length !== -1 &&
+        comments.map((c) => (
+          <div>
+            <Row>
+              <Col xs="1">
+                 <img 
+                    className="comment-image"
+                    src={props.element.user.image}
+                    style={{borderRadius: 50}}/> 
+              </Col>
+              <Col xs="11">
+                <div className="ml-2 commentBox d-flex">
+                  <div className="">
+                    {" "}
+                    {props.element.user.name}
+                    {props.element.user.surname} {" "}
+                  </div>
+                  <div className="ml-2 py-0 comment-title">
+                    {" "}
+                    {props.element.user.title}{" "}
+                  </div>
+                  <div className="ml-2 py-0 commentText">{c.comment}</div>
+                </div>
+                <div className="d-flex ml-auto"></div>
+              </Col>
+            </Row>
+          </div>
+        ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
